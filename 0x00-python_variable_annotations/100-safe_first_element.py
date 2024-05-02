72731f6e4f07302882e933ee534da361b65f53ef
Ben Kimani <benzoic09@gmail.com>
2024-05-02 14:14:10 +0000

    task 100

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """add duck-typed annotated """
-from typing import Any
+from typing import Any, sequence, Union
 
 
-def safe_first_element(lst: list) -> Any:
+def safe_first_element(lst:  Sequence[Any]) -> Union[Any, None]:
     """add duck-typed annotated """
     if lst:
         return lst[0]
