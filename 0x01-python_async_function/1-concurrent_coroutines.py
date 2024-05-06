33943ee3b4e4aaf950196ea139aee7ffb0fc3b3e
Ben Kimani <benzoic09@gmail.com>
2024-05-06 14:05:08 +0000

    task one

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" that takes in 2 int arguments (in this order):
+n and max_delay"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """takes 2 args"""
+
