c131f859a5b3d791e7642e27fbb30f5e5dfce2b8
Ben Kimani <benzoic09@gmail.com>
2024-05-27 18:32:17 +0000

    unit test task 1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -18,3 +18,18 @@ class TestAccessNestedMap(unittest.TestCase):
         the expected results"""
         self.assertEqual(access_nested_map(
             nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+        ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """Test that access_nested_map raises KeyError as expected"""
+        with self.assertRaises(KeyError) as cm:
+            access_nested_map(nested_map, path)
+
+            self.assertEqual(cm.exception.args[0], path[-1])
+
+
+if __name__ == "__main__":
+    unittest.main()
